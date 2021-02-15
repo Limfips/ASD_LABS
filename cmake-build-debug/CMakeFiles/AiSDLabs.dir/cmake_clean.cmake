81b23a20193849b6d04a4be3d76d25f66e0c74ab
Limfips <limfips@gmail.com>
2021-02-15 18:58:20 +0400

    init lab1

---
+++ cmake-build-debug/CMakeFiles/AiSDLabs.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "AiSDLabs.exe"
+  "AiSDLabs.exe.manifest"
+  "AiSDLabs.pdb"
+  "CMakeFiles/AiSDLabs.dir/main.cpp.obj"
+  "libAiSDLabs.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AiSDLabs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
