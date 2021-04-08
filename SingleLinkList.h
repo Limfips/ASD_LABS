27f9ba6b9b75c6d2e70af3d7a2c86d58d489ed58
Leonid <dudka.leonid@gmail.com>
2021-04-08 11:43:18 +0400

    создание списков

---
+++ SingleLinkList.h
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+struct Student{
+    long id;
+    char firstName[10];
+    const char *secondName;
+    long *pRatings;
+    int count;
+};
+
+class SingleLinkList {
+
+private: struct Link {
+        Link *next;
+        Student *value;
+    };
+
+private: Link* header;
+
+public: SingleLinkList() {
+    header = new Link{nullptr, nullptr};
+}
+
+private: static void printStudentInfo(Student *student) {
+        cout << student->id << ": " << student->firstName << " " << student->secondName << " ";
+    }
+
+public: void showList() {
+        Link *q = header->next;
+        while (q) {
+            cout << "address: " << q << " | next: " << q->next  << " | value: (";
+            printStudentInfo(q->value);
+            cout << ")" << endl;
+            q = q->next;
+        }
+        cout << endl;
+    }
+
+public: Link* addItem(Student *value) {
+        Link *q = header;
+        while (q->next != nullptr) { q = q->next; };
+        Link *nextLink = new Link{nullptr, value};
+        q->next = nextLink;
+        return header;
+    }
+
+public: Link* deleteItem(Student *value) {
+        Link *q = header;
+        while (q->next->value->id != value->id) { q = q->next; };
+        Link *tmp = q->next;
+        q->next = tmp->next;
+        free(tmp);
+        return header;
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include "SingleLinkList.h"
 
 
 int main() {
+
+
+    SingleLinkList linkedList = SingleLinkList();
+
+    Student student1 = Student{1, "FN1", "SN1"};
+    Student student2 = Student{2, "FN2", "SN2"};
+    Student student3 = Student{3, "FN3", "SN3"};
+    Student student4 = Student{4, "FN4", "SN4"};
+    Student student5 = Student{5, "FN5", "SN5"};
+
+    linkedList.addItem(&student1);
+    linkedList.addItem(&student2);
+    linkedList.addItem(&student3);
+    linkedList.addItem(&student4);
+    linkedList.addItem(&student5);
+
+    char* list1 = new char[6] {'f', 'd', 'a', 'c', 'b', 'e'};
+
 
 
     return 0;
