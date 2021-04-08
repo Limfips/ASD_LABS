366d49ac382607de81764537740c6d3fd8198fee
Leonid <dudka.leonid@gmail.com>
2021-04-08 19:08:45 +0400

    v0.3

---
+++ TreeList.h
@@ -0,0 +1,214 @@
+using namespace std;
+
+struct Student{
+    long id;
+    char firstName[10];
+    const char *secondName;
+    long *pRatings;
+    int count;
+};
+
+class TreeList {
+
+private:
+    struct Node
+    {
+        int key; // Ключ, по которому строится дерево
+        Student* data; // Полезные данные (любого типа)
+        Node *left, *right; // Указатели на соседние узлы
+    };
+
+    Node* root;
+
+    bool addNode(Node*& node, int key, Student* data) {
+        if (node == nullptr) {
+            node = new Node{key, data, nullptr, nullptr};
+            return true;
+        }
+        if (node->key > key) {
+            return addNode(node->left, key, data);
+        }
+        if (node->key < key) {
+            return addNode(node->right, key, data);
+        }
+        return false;
+    }
+
+    void inorder(Node* r)
+    {
+        if (r == nullptr) return;
+        inorder(r->left);
+        cout<<r->key<<":["<<r->data->id<<"] ";
+        inorder(r->right);
+    }
+
+    void preorder(Node* r)
+    {
+        if (r == nullptr) return;
+        cout<<r->key<<":["<<r->data->id<<"] ";
+        preorder(r->left);
+        preorder(r->right);
+    }
+
+    void postorder(Node* r)
+    {
+        if (!r) return;
+        postorder(r->left);
+        postorder(r->right);
+        cout<<r->key<<":["<<r->data->id<<"] ";
+    }
+
+    Student* searchByKeyNode(Node* r, int key)
+    {
+        if (r == nullptr) {
+            return nullptr;
+        }
+        if (r->key == key) {
+            return r->data;
+        }
+        if (r->key > key) {
+            return searchByKeyNode(r->left, key);
+        }
+        if (r->key < key) {
+            return searchByKeyNode(r->right, key);
+        }
+        return nullptr;
+    }
+
+    bool dellNode(Node *r, Node* parent) {
+        Node* tmp = r->right;
+        Node* tmpParent = r;
+        while (tmp->left != nullptr) {
+            tmpParent = tmp;
+            tmp = tmp->left;
+        }
+        if (parent == nullptr) {
+            cout << endl;
+            cout << "deleteNode: " << r->key << endl;
+            cout << "tmp: " << tmp->key << endl;
+
+            tmpParent->left = tmp->right;
+            tmp->left = r->left;
+            tmp->right = r->right;
+            free(r);
+            root = tmp;
+        } else {
+            cout << endl;
+            cout << "parent: " << parent->key << endl;
+            cout << "deleteNode: " << r->key << endl;
+            cout << "tmp: " << tmp->key << endl;
+
+            parent->right = tmp;
+
+            tmpParent->left = tmp->right;
+            tmp->left = r->left;
+            tmp->right = r->right;
+            free(r);
+        }
+
+        return true;
+    }
+    bool removeNode(Node *r, int key, Node* parent) {
+        if (r == nullptr) {
+            return false;
+        }
+        if (r->key == key) {
+            dellNode(r, parent);
+            return true;
+        } else {
+            if (r->key > key) {
+                return removeNode(r->left, key, r);
+            }
+            if (r->key < key) {
+                return removeNode(r->right, key, r);
+            }
+        }
+        return false;
+    }
+
+
+public:
+    TreeList() {
+        root = nullptr;
+    }
+
+    bool add(int key, Student* data) {
+        if (root == nullptr) {
+            root = new Node{key, data, nullptr, nullptr};
+            return true;
+        }
+        if (root->key > key) {
+            return addNode(root->left, key, data);
+        }
+        if (root->key < key) {
+            return addNode(root->right, key, data);
+        }
+        return false;
+    }
+
+    bool remove(int key) {
+       if (root == nullptr) {
+           return false;
+       }
+        if (root->key == key) {
+            dellNode(root, nullptr);
+            return true;
+        } else {
+            if (root->key > key) {
+                return removeNode(root->left, key, root);
+            }
+            if (root->key < key) {
+                return removeNode(root->right, key, root);
+            }
+        }
+        return false;
+    }
+
+    Student* searchByKey(int key) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+        if (root->key == key) {
+            return root->data;
+        }
+        if (root->key > key) {
+            return searchByKeyNode(root->left, key);
+        }
+        if (root->key < key) {
+            return searchByKeyNode(root->right, key);
+        }
+        return nullptr;
+    }
+
+    // в глубину - Последовательный
+    // . Последовательный (он же инфиксный, симметричный,
+    //  поперечный) – дерево проходится, начиная с левой ветви вверх к
+    //  корню, затем к правой ветви;
+    void print1() {
+        if (root == nullptr) return;
+        inorder(root->left);
+        cout<<root->key<<" ";
+        inorder(root->right);
+    }
+
+    // в глубину - Нисходящий
+    // . Последовательный (он же инфиксный, симметричный,
+    //  поперечный) – дерево проходится, начиная с левой ветви вверх к
+    //  корню, затем к правой ветви;
+    void print2() {
+        if (root == nullptr) return;
+        cout<<root->key<<" ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+
+    // в глубину - Восходящий
+    //  Восходящий (постфиксный или обратный) – проходится
+    //  левая ветвь, затем правая, затем корень.
+    void print3() {
+        if (!root) return;
+        postorder(root->left);
+        postorder(root->right);
+        cout<<root->key<<":["<<root->data->id<<"] ";
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include "TreeList.h"
 
+using namespace std;
+
+void printStudentInfo(Student* student){
+    if (student == nullptr) return;
+    cout
+            << "address: " << student
+            << " | " <<  student->id
+            << endl;
+}
 
 int main() {
 
+    TreeList list = TreeList();
+    int* args = new int[13]{70, 60, 85, 87, 90, 45, 30, 88, 35, 20, 86, 82, 83};
+
+    for (int i = 0; i < 13; i++) {
+        list.add(args[i], new Student{args[i] * 10});
+    }
+
+    list.print1();
+    cout << endl;
+    list.print2();
+    cout << endl;
+    list.print3();
+    cout << endl;
+
+    printStudentInfo(list.searchByKey(90));
+    printStudentInfo(list.searchByKey(12313));
+
+    cout << "test delete_1: ";
+    list.remove(85);
+    list.print2();
+    cout << "test delete_2: ";
+    list.remove(3093);
 
     return 0;
 }
